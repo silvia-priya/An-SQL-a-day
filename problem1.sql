1ad455872de21f1aa757cb020786936f4e8b5b6a
Silvia Priyadharshini <67099867+silvia-priya@users.noreply.github.com>
2024-12-11 08:51:10 +0530

    Update problem1.sql

--- problem1.sql
+++ problem1.sql
@@ -1,7 +1,7 @@
-Recyclable and low fat products
+-- Recyclable and low fat products
 
 select product_id
 from
 Products
-where low_fats='Y' and recyclable='Y'
-order by product_id;
+where low_fats='Y' and recyclable='Y' -- where condition helps to check for a condition
+order by product_id; -- order by helps to arrange the rows based on the specified column, default is ascending order
